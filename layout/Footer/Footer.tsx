3b27c6bc27d9de765d11c09069eb577cd71d952f
Anton Dymov <dymov.ant@gmail.com>
2021-12-11 23:43:58 +0500

    Верстка layout и footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,9 +1,15 @@
+import cn from "classnames";
+import {format} from "date-fns";
 import { FooterProps } from "./Footer.props";
+import { Text } from "../../components/UI";
+import styles from "./Footer.module.css";
 
-export const Footer = (props: FooterProps): JSX.Element => {
+export const Footer = ({className, ...props}: FooterProps): JSX.Element => {
   return (
-    <footer { ...props }>
-      Footer
+    <footer { ...props } className={cn(className, styles.footer)}>
+      <Text className={styles.copy}>OwlTop © 2020 - {format(new Date(), "yyyy")} Все права защищены</Text>
+      <a className={styles.link} target="_blank" href="#">Пользовательское соглашение</a>
+      <a className={styles.link} target="_blank" href="#">Политика конфиденциальности</a>
     </footer>
   );
 };
--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -2,18 +2,17 @@ import { LayoutProps } from "./Layout.props";
 import { Header } from "./Header/Header";
 import { Sidebar } from "./Sidebar/Sidebar";
 import { Footer } from "./Footer/Footer";
+import styles from "./Layout.module.css";
 
 export const Layout = ({children}: LayoutProps): JSX.Element => {
   return (
-    <>
-      <Header/>
-      <div>
-        <Sidebar/>
-        <div>
-          { children }
-        </div>
+    <div className={styles.wrapper}>
+      <Header className={styles.header}/>
+      <Sidebar className={styles.sidebar}/>
+      <div className={styles.body}>
+        { children }
       </div>
-      <Footer/>
-    </>
+      <Footer className={styles.footer}/>
+    </div>
   );
 };
